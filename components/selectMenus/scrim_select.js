ef407989cfe48c6c469fdd653b351faad4d6e4e6
jaded-x <bayne.jaded@gmail.com>
2022-09-27 15:17:22 +1000

    added select count

--- components/selectMenus/scrim_select.js
+++ components/selectMenus/scrim_select.js
@@ -5,16 +5,21 @@ const { Role } = require('../../util/roles');
 module.exports = {
     name: 'scrim_select',
     async execute(interaction) {
-        const embed = interaction.message.embeds[0].toJSON()
+        var embed = interaction.message.embeds[0].toJSON()
         const role = Role[fs.readFileSync(`./data/users/${interaction.member.id}/role.txt`, 'utf-8')];
+        const option = Options[interaction.values[0]];
 
         for (i = 1; i <= 3; i++) {
             embed.fields[i].value = embed.fields[i].value.replaceAll(`\n${role}┃${interaction.member}`, '');
             embed.fields[i].value = embed.fields[i].value.replaceAll(`${role}┃${interaction.member}`, '');
         }
 
-        if (embed.fields[Options[interaction.values[0]]].value === '** **') embed.fields[Options[interaction.values[0]]].value = `${embed.fields[Options[interaction.values[0]]].value}${role}┃${interaction.member}`
-        else embed.fields[Options[interaction.values[0]]].value = `${embed.fields[Options[interaction.values[0]]].value}\n${role}┃${interaction.member}`
+        if (embed.fields[option].value === '** **') embed.fields[option].value = `${embed.fields[option].value}${role}┃${interaction.member}`
+        else embed.fields[option].value = `${embed.fields[option].value}\n${role}┃${interaction.member}`
+
+        for (i = 1; i <= 3; i++) {
+            embed.fields[i].name = embed.fields[i].name.replace(embed.fields[i].name.match(/\d+/)[0], embed.fields[i].value.replace(/[^┃]/g, '').length);
+        }
 
 
         const editedEmbed = EmbedBuilder.from(embed);
